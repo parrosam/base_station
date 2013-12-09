d3ed81782751184706333e46c6ad00057b4b647e
unknown <rwooster@eecs373-10.adsroot.itcs.umich.edu>
2013-12-08 20:38:29 -0500

    Timers fixed bidirectional xbee communication

--- SoftConsole/controller_MSS_MSS_CM3_0/controller_MSS_MSS_CM3_0_app/Debug/main.d
+++ SoftConsole/controller_MSS_MSS_CM3_0/controller_MSS_MSS_CM3_0_app/Debug/main.d
@@ -4,7 +4,9 @@ main.d main.o: ../main.c ../controller.h ../display.h \
  C:\Users\rwooster\Documents\base_station\SoftConsole\controller_MSS_MSS_CM3_0\controller_MSS_MSS_CM3_0_hw_platform/drivers/mss_uart/mss_uart.h \
  C:\Users\rwooster\Documents\base_station\SoftConsole\controller_MSS_MSS_CM3_0\controller_MSS_MSS_CM3_0_hw_platform/drivers/mss_uart/../../CMSIS/a2fxxxm3.h \
  C:\Users\rwooster\Documents\base_station\SoftConsole\controller_MSS_MSS_CM3_0\controller_MSS_MSS_CM3_0_hw_platform/drivers/mss_uart/../../CMSIS/core_cm3.h \
- C:\Users\rwooster\Documents\base_station\SoftConsole\controller_MSS_MSS_CM3_0\controller_MSS_MSS_CM3_0_hw_platform/drivers/mss_uart/../../CMSIS/system_a2fxxxm3.h
+ C:\Users\rwooster\Documents\base_station\SoftConsole\controller_MSS_MSS_CM3_0\controller_MSS_MSS_CM3_0_hw_platform/drivers/mss_uart/../../CMSIS/system_a2fxxxm3.h \
+ C:\Users\rwooster\Documents\base_station\SoftConsole\controller_MSS_MSS_CM3_0\controller_MSS_MSS_CM3_0_hw_platform/drivers/mss_timer/mss_timer.h \
+ C:\Users\rwooster\Documents\base_station\SoftConsole\controller_MSS_MSS_CM3_0\controller_MSS_MSS_CM3_0_hw_platform/drivers/mss_timer/../../CMSIS/a2fxxxm3.h
 
 ../controller.h:
 
@@ -21,3 +23,7 @@ C:\Users\rwooster\Documents\base_station\SoftConsole\controller_MSS_MSS_CM3_0\co
 C:\Users\rwooster\Documents\base_station\SoftConsole\controller_MSS_MSS_CM3_0\controller_MSS_MSS_CM3_0_hw_platform/drivers/mss_uart/../../CMSIS/core_cm3.h:
 
 C:\Users\rwooster\Documents\base_station\SoftConsole\controller_MSS_MSS_CM3_0\controller_MSS_MSS_CM3_0_hw_platform/drivers/mss_uart/../../CMSIS/system_a2fxxxm3.h:
+
+C:\Users\rwooster\Documents\base_station\SoftConsole\controller_MSS_MSS_CM3_0\controller_MSS_MSS_CM3_0_hw_platform/drivers/mss_timer/mss_timer.h:
+
+C:\Users\rwooster\Documents\base_station\SoftConsole\controller_MSS_MSS_CM3_0\controller_MSS_MSS_CM3_0_hw_platform/drivers/mss_timer/../../CMSIS/a2fxxxm3.h:
